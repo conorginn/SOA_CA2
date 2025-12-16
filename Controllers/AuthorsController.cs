347a4c1de1cf50cdc9f75f4bcf896c90a0e6bee3
xconor <d00261104@student.dkit.ie>
2025-12-16 20:47:09 +0000

    Add project files.

---
+++ Controllers/AuthorsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorsController : ControllerBase
+    {
+    [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateAuthorDto dto)
+        {
+            return CreatedAtAction(nameof(GetById), new { id = 1 }, null);
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> Update(int id, [FromBody] UpdateAuthorDto dto)
+        {
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            return NoContent();
+        }
+    }
+}
+
